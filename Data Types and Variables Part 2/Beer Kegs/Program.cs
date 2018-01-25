d2c7fe0f9fd133d36bcd35cfe2f2c24dd9b3cb25
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2018-01-25 16:52:04 +0200

    Add files via upload

---
+++ Data Types and Variables Part 2/Beer Kegs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beer_Kegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countGroup = int.Parse(Console.ReadLine());
+
+            string size = "";
+            double min = double.MinValue;
+
+            for (int i = 0; i < 3 * countGroup; i += 3)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double volume = Math.PI * Math.Pow(radius, 2) * height;
+                if (volume > min)
+                {
+                    min = volume;
+                    size = model;
+                }
+            }
+            Console.WriteLine(size);
+        }
+    }
+}
+    
