57ca25ab9f869ab5a1c869f1470b791b2ebcda1f
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2018-01-25 15:28:18 +0200

    Add files via upload

---
+++ Conditional Statements and Loops/Test Numbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int testNumber = int.Parse(Console.ReadLine());
+
+            int counterCombinations = 0;
+            int product = 0;
+
+            for (int i = firstNumber; i >= 1; i--)
+            {
+                for (int k = 1; k <= secondNumber; k++)
+                {
+                    counterCombinations++;
+                    product += 3 * (i * k);
+
+                    if (product >= testNumber)
+                    {
+                        Console.WriteLine($"{counterCombinations} combinations");
+                        Console.WriteLine($"Sum: {product} >= {testNumber}");
+                        return;
+                    }
+                }
+            }
+
+            if (product < testNumber)
+            {
+                Console.WriteLine($"{counterCombinations} combinations");
+                Console.WriteLine($"Sum: {product}");
+            }
+        }
+    }
+}
