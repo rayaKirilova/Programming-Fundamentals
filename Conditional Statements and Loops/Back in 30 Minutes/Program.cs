7fce6d2e3c798354cb64494b494e83a436653a8f
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2018-01-25 15:36:18 +0200

    Add files via upload

---
+++ Conditional Statements and Loops/Back in 30 Minutes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Back_in_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hours = int.Parse(Console.ReadLine());
+            var minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+
+            while (minutes >= 60)
+            {
+                minutes -= 60;
+                hours++;
+            }
+
+            while (hours >= 24)
+            {
+                hours -= 24;
+            }
+
+            Console.WriteLine($"{hours}:{minutes:D2}");
+        }
+    }
+}
