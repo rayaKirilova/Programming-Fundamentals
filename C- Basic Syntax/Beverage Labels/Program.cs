2db6c8f088d5b8ddcb818ae84c2f2a638aca7e39
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2018-01-25 14:00:05 +0200

    Add files via upload

---
+++ C- Basic Syntax/Beverage Labels/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            double volume = double.Parse(Console.ReadLine());
+            double energyContent = double.Parse(Console.ReadLine());
+            double sugarContent = double.Parse(Console.ReadLine());
+
+            double volumePerMl = volume / 100;
+            double productKcal = energyContent * volumePerMl;
+            double productSugars = sugarContent * volumePerMl;
+
+            Console.WriteLine(volume + "ml " + product + ":");
+            Console.WriteLine("{0}kcal, {1}g sugars", productKcal, productSugars);
+        }
+    }
+}
