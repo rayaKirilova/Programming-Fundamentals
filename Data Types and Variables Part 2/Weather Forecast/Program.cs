94da624575688af3048627a93f5b16af839f81dc
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2018-01-25 16:50:57 +0200

    Add files via upload

---
+++ Data Types and Variables Part 2/Weather Forecast/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather_Forecast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberToCheck = double.Parse(Console.ReadLine());
+
+            if (numberToCheck % 1 == 0)
+            {
+                if (numberToCheck >= sbyte.MinValue && numberToCheck <= sbyte.MaxValue)
+                {
+                    Console.WriteLine("Sunny");
+                }
+                else if (numberToCheck >= int.MinValue && numberToCheck <= int.MaxValue)
+                {
+                    Console.WriteLine("Cloudy");
+                }
+                else if (numberToCheck >= long.MinValue && numberToCheck <= long.MaxValue)
+                {
+                    Console.WriteLine("Windy");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Rainy");
+            }
+        }
+    }
+}
