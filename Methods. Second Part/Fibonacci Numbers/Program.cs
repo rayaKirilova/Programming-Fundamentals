5c413d02ee33530e652ab422cfed3900edf9d3f7
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2018-01-27 15:00:11 +0200

    Add files via upload

---
+++ Methods. Second Part/Fibonacci Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int fibNumber = Fibonacci(n);
+
+            Console.WriteLine(fibNumber);
+        }
+
+        private static int Fibonacci(int n)
+        {
+            int fibNumber = 1;
+            int fib1 = 0;
+            int fib2 = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                fibNumber = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = fibNumber;
+            }
+            return fibNumber;
+        }
+    }
+}
+    
