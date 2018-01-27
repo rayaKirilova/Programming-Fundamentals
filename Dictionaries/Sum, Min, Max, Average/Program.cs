265d6abb35c032b81341d7c09e9821ff7d26af82
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2018-01-27 18:45:18 +0200

    Add files via upload

---
+++ Dictionaries/Sum, Min, Max, Average/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum__Min__Max__Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Sum = {0}", numbers.Sum());
+            Console.WriteLine("Min = {0}", numbers.Min());
+            Console.WriteLine("Max = {0}", numbers.Max());
+            Console.WriteLine("Average = {0}", numbers.Average());
+        }
+    }
+}
