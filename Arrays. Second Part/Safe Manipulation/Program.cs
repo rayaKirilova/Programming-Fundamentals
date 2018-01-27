a403123f2d47ef95f703cb3201a5bf1d0aecb45d
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2018-01-27 17:05:34 +0200

    Add files via upload

---
+++ Arrays. Second Part/Safe Manipulation/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safe_Manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine()
+           .Split()
+           .ToList();
+
+            List<string> commands = Console.ReadLine().Split(' ').ToList();
+
+            while (commands[0] != "END")
+            {
+
+                if (commands[0] == "Reverse")
+                {
+                    words.Reverse();
+                }
+                else if (commands[0] == "Distinct")
+                {
+                    words = words.Distinct().ToList();
+                }
+                else if (commands[0] == "Replace")
+                {
+                    int index = int.Parse(commands[1]);
+                    string forReplace = commands[2];
+
+                    if (index >= 0 && index < words.Count)
+                    {
+                        words[index] = forReplace;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+                commands = Console.ReadLine().Split(' ').ToList();
+            }
+            Console.WriteLine(string.Join(", ", words));
+        }
+    }
+}
+    
