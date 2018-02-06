5eccf5467df9fdc7394c23a5e22ceea1cddba9d8
Raya Nikolova- Kirilova <32265273+rayaKirilova@users.noreply.github.com>
2018-02-06 15:33:57 +0200

    Add files via upload

---
+++ RegEx/Match Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions; 
+
+namespace Match_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))"; // possitive lookbehind & possitive lookahead !!
+
+            string nums = Console.ReadLine();
+
+            MatchCollection matchedNums = Regex.Matches(nums, pattern);
+
+            foreach (Match item in matchedNums)
+            {
+                Console.Write(item.Value + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ RegEx/Match Phone Number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions; 
+
+namespace Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\+359 2 \d{3} \d{4}\b)|(\+359-2-\d{3}-\d{4}\b)";
+            string phoneNumbers = Console.ReadLine();
+
+            MatchCollection phones = Regex.Matches(phoneNumbers, pattern);
+
+            var phonesToPrint = phones.Cast<Match>().Select(a => a.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(", ", phonesToPrint));
+        }
+    }
+}
